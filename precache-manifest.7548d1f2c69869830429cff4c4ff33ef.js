852062e84edb9303a4324fbf37da752fe0a68500
umakant <umakantsonwani@gmail.com>
2019-01-26 00:40:27 +0530

    Upload new Build

---
+++ precache-manifest.7548d1f2c69869830429cff4c4ff33ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "307d1f4d58d19e59ad00",
+    "url": "/static/js/main.307d1f4d.chunk.js"
+  },
+  {
+    "revision": "a60233bc1ca5e9e8fa0e",
+    "url": "/static/js/1.a60233bc.chunk.js"
+  },
+  {
+    "revision": "307d1f4d58d19e59ad00",
+    "url": "/static/css/main.eac69930.chunk.css"
+  },
+  {
+    "revision": "94992782d719b41fa119f77374fe22c3",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c31a2e56a835b747bbfc78822acd4401.js"
+  "/precache-manifest.7548d1f2c69869830429cff4c4ff33ef.js"
 );
 
 workbox.clientsClaim();
